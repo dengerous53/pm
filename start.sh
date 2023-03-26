d3c9b5ad44d8a15c6590bcb117617d2cd368d7d7
ʜᴀʀɪ <127658855+HS-BOTZ@users.noreply.github.com>
2023-03-26 23:34:36 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,12 @@
-cd FilterBot
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/HS-BOTZ/pm.git /moviefinderextra
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /moviefinderextra
+fi
+cd /moviefinderextra
 pip3 install -U -r requirements.txt
-echo "Starting Bot...."
+echo "Starting Bot....💥"
 python3 bot.py
